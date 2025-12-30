ee6ea0021b0e48f6eca3123c5dac7816f487c721
陈兆杰 <15298506403@163.com>
2025-12-30 13:13:06 +0800

    commit

--- FlaUISample/mainForm.Designer.cs
+++ FlaUISample/mainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FlaUISample
 {
-    partial class Form1
+    partial class mainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnStart = new Button();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(149, 78);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(75, 23);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // mainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(509, 390);
+            Controls.Add(btnStart);
+            Name = "mainForm";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnStart;
     }
 }
---
+++ FlaUISample/mainForm.cs
@@ -0,0 +1,50 @@
+﻿using FlaUI.UIA3;
+
+namespace FlaUISample
+{
+    /// <summary>
+    /// FlaUI：UI 自动化库
+    /// https://github.com/FlaUI/FlaUI
+    /// FlaUInspect：一个基于 FlaUI 的 UI 自动化检查工具
+    /// https://github.com/FlaUI/FlaUInspect
+    /// </summary>
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // 1️⃣ 启动应用
+            string exePath = @"C:\Program Files\Notepad++\notepad++.exe";
+            FlaUI.Core.Application app = null;
+            while (app == null)
+            {
+                try
+                {
+                    // 尝试 Attach 已启动进程
+                    app = FlaUI.Core.Application.Attach("notepad++");
+                }
+                catch
+                {
+                    // 没有启动就 Launch
+                    try
+                    {
+                        app = FlaUI.Core.Application.Launch(exePath);
+                    }
+                    catch
+                    {
+                        // 程序还没准备好
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+
+            using var automation = new UIA3Automation();
+            var mainWindow = app.GetMainWindow(automation);
+            Console.WriteLine("窗口标题：" + mainWindow.Title);
+        }
+    }
+}
